242153802dd3c29a986a968f5cb494ccbeb7f521
johanhammerin <johan04hammerin@gmail.com>
2025-08-27 21:36:34 +0200

    small fix for turn-in

--- lektion-5/src/main/java/se/johan/lektion_5/controller/UserController.java
+++ lektion-5/src/main/java/se/johan/lektion_5/controller/UserController.java
@@ -1,10 +1,14 @@
 package se.johan.lektion_5.controller;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.johan.lektion_5.model.CustomUser;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -40,5 +44,30 @@ public class UserController {
         return ResponseEntity.internalServerError().build();
     }
 
+
+    List<CustomUser> userList = new ArrayList<>(
+            List.of(
+                    new CustomUser(1, "Johan", "123", true),
+                    new CustomUser(2, "Johan", "123", false),
+                    new CustomUser(3, "123", "Johan", true),
+                    new CustomUser(4, "Benny", "456", true),
+                    new CustomUser(5, "456", "Benny", true),
+                    new CustomUser(6, "admin", "0", true)
+            )
+    );
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<String> deleteUser(@PathVariable("username") String username) {
+        CustomUser userToBeDeleted = null;
+        for (int i = 0; i < userList.size(); i++) {
+            if (Objects.equals(userList.get(i).username(), username)) {
+                userToBeDeleted = userList.get(i);
+                userList.remove(i);
+                return ResponseEntity.ok().body(username + " was deleted");
+            }
+
+        }
+        return ResponseEntity.status(404).build();
+    }
 
 }
--- lektion-5/src/main/java/se/johan/lektion_5/model/CustomUser.java
+++ lektion-5/src/main/java/se/johan/lektion_5/model/CustomUser.java
@@ -1,4 +1,8 @@
 package se.johan.lektion_5.model;
 
-public record CustomUser(String username, String password) {
+public record CustomUser(
+        int id,
+        String username,
+        String password,
+        boolean isEnabled) {
 }
